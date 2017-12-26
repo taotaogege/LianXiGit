66568cc7a56d7f23fa717c47332031ec32d0a4ce
yinyulong <alongstorm@163.com>
2017-12-26 10:27:37 +0800

    新项目

--- app/src/main/java/com/bwie/lianxigit/activity/MainActivity.java
+++ app/src/main/java/com/bwie/lianxigit/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bwie.lianxigit;
+package com.bwie.lianxigit.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bwie.lianxigit.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/bwie/lianxigit/bean/User.java
+++
@@ -1,9 +0,0 @@
-package com.bwie.lianxigit.bean;
-
-/**
- * Created by lenovo on 2017/12/20.
- */
-
-public class User {
-
-}
