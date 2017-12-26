c94350bc5612518d833508836834d69f36d9b411
yinyulong <alongstorm@163.com>
2017-12-26 14:40:22 +0800

    新增了截屏功能

--- app/src/main/java/com/bwie/lianxigit/activity/MainActivity.java
+++ app/src/main/java/com/bwie/lianxigit/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.bwie.lianxigit.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.androidyuan.lib.screenshot.ScreenShotActivity;
 import com.bwie.lianxigit.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn_go);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ScreenShotActivity.class));
+            }
+        });
     }
 }
